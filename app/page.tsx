1ec7af091f3c1662e95b810477903b3260a9b398
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2024-09-17 10:51:03 -0400

    add login image, copyright and admin link

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
-import { Button } from "@/components/ui/button";
 import Image from "next/image";
+
+// Component Imports
+import PatientForm from "@/components/forms/PatientForm";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -13,8 +17,24 @@ export default function Home() {
             width={1000}
             className="mb-12 h-10 w-fit"
           />
+          <PatientForm />
+          <div className="text-14-regular mt-20 flex justify-between">
+            <p className="justify-items-end text-dark-600 xl:text-left">
+              © 2024 CarePulse
+            </p>
+            <Link href="/?admin=true" className="text-green-500">
+              Admin
+            </Link>
+          </div>
         </div>
       </section>
+      <Image
+        src="/assets/images/onboarding-img.png"
+        alt="patient"
+        height={1000}
+        width={1000}
+        className="side-img max-w-[50%]"
+      />
     </div>
   );
 }
