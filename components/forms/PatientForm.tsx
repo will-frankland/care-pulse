5d3c75184642d410b4f2db5ee0da834aa6ed8229
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2024-09-19 10:27:37 -0400

    create custom button

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -6,6 +6,8 @@ import { z } from "zod";
 import { Button } from "@/components/ui/button";
 import { Form } from "@/components/ui/form";
 import CustomFormField from "../CustomFormField";
+import SubmitButton from "../SubmitButton";
+import { useState } from "react";
 
 export enum FormFieldType {
   INPUT = 'input',
@@ -24,6 +26,9 @@ const formSchema = z.object({
 });
 
 const PatientForm = () => {
+
+  const [isLoading, setIsLoading] = useState(false)
+
   // 1. Define your form.
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
@@ -70,7 +75,7 @@ const PatientForm = () => {
           label="Phone number"
           placeholder="(555) 123-4567"
         />
-        <Button type="submit">Submit</Button>
+        <SubmitButton isLoading={isLoading}>Get Started</SubmitButton>
       </form>
     </Form>
   );
