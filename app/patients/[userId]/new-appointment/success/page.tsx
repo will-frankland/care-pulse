0c8447811d27863f3389b18d2fff127d8b304f6c
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2024-09-26 11:54:26 -0400

    add searchParams to get appointment and doctor info for success page

--- app/patients/[userId]/new-appointment/success/page.tsx
+++ app/patients/[userId]/new-appointment/success/page.tsx
@@ -1,8 +1,16 @@
+import { Doctors } from "@/constants";
+import { getAppointment } from "@/lib/actions/appointment.actions";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
-const SuccessPage = () => {
+const SuccessPage = async ({ params: { userId }, searchParams }: SearchParamProps) => {
+
+  const appointmentId = (searchParams?.appointmentId as string || "");
+  const appointment = await getAppointment(appointmentId);
+
+  const doctor = Doctors.find((doc) => doc.name === appointment.primaryPhysician);
+
   return (
     <div className="flex h-screen max-h-screen px-[5%]">
       <div className="success-img">
@@ -31,9 +39,13 @@ const SuccessPage = () => {
         <section className="request-details">
           <p>Requested appointment details:</p>
           <div className="flex items-center gap-3">
-            {/* <Image
-
-            /> */}
+            <Image
+              src={doctor?.image!}
+              alt={"doctor"}
+              width={100}
+              height={100}
+              className="size-6"
+            />
           </div>
         </section>
       </div>
