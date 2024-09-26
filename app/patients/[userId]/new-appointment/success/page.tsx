30b9e58df328e5aa9d21d80621a6edc5a475e49a
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2024-09-26 11:45:07 -0400

    setup initial styling of success page

---
+++ app/patients/[userId]/new-appointment/success/page.tsx
@@ -0,0 +1,44 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const SuccessPage = () => {
+  return (
+    <div className="flex h-screen max-h-screen px-[5%]">
+      <div className="success-img">
+        <Link href="/">
+          <Image
+            src="/assets/icons/logo-full.svg"
+            alt="logo"
+            height={1000}
+            width={1000}
+            className="h-10 w-fit"
+          />
+        </Link>
+        <section className="flex flex-col items-center">
+          <Image
+            src="/assets/gifs/success.gif"
+            alt="success"
+            height={300}
+            width={280}
+          />
+          <h2 className="header mb-6 max-w-[600px] text-center">
+            Your <span className="text-green-500">appointment request</span> has
+            been successfully submitted!
+          </h2>
+          <p>We will be in touch shortly to confirm.</p>
+        </section>
+        <section className="request-details">
+          <p>Requested appointment details:</p>
+          <div className="flex items-center gap-3">
+            {/* <Image
+
+            /> */}
+          </div>
+        </section>
+      </div>
+    </div>
+  );
+};
+
+export default SuccessPage;
