fb078fcc6b70ef30197cfe7eff0c7db4d85ef715
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2024-09-26 19:57:39 -0400

    add twilio integration

--- lib/actions/appointment.actions.ts
+++ lib/actions/appointment.actions.ts
@@ -4,8 +4,9 @@ import {
   APPOINTMENT_COLLECTION_ID,
   DATABASE_ID,
   databases,
+  messaging,
 } from "../appwrite.config";
-import { parseStringify } from "../utils";
+import { formatDateTime, parseStringify } from "../utils";
 import { Appointment } from "@/types/appwrite.types";
 import { revalidatePath } from "next/cache";
 
@@ -80,24 +81,53 @@ export const getRecentAppointmentList = async () => {
   }
 };
 
-export const updateAppointment = async ({ appointmentId, userId, appointment, type }: UpdateAppointmentParams) => {
+export const updateAppointment = async ({
+  appointmentId,
+  userId,
+  appointment,
+  type,
+}: UpdateAppointmentParams) => {
   try {
     const updatedAppointment = await databases.updateDocument(
       DATABASE_ID!,
       APPOINTMENT_COLLECTION_ID!,
       appointmentId,
       appointment
-    )
+    );
 
-    if(!updatedAppointment) {
-      throw new Error('Appointment not found')
+    if (!updatedAppointment) {
+      throw new Error("Appointment not found");
     }
 
-    // SMS Notification
+    const smsMessage = `
+      Hi, it's CarePulse.
+      ${
+        type === "schedule"
+          ? `Your appointment has been scheduled for ${formatDateTime(
+              appointment.schedule!
+            ).dateTime} with Dr. ${appointment.primaryPhysician}`
+          : `We regret to inform you that your appointment has been cancelled for the following reason: ${appointment.cancellationReason}`
+      }`;
 
-    revalidatePath('/admin');
-    return parseStringify(updatedAppointment)
+    await sendSMSNotification(userId, smsMessage);
+
+    revalidatePath("/admin");
+    return parseStringify(updatedAppointment);
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-}
+};
+
+export const sendSMSNotification = async (userId: string, content: string) => {
+  try {
+    const message = await messaging.createSms(
+      ID.unique(),
+      content,
+      [],
+      [userId]
+    );
+    return parseStringify(message);
+  } catch (error) {
+    console.log(error);
+  }
+};
