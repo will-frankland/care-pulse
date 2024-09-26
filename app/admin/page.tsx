a66c38e80b98908a93bb7a3064dc20e4f59f5762
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2024-09-26 13:48:02 -0400

    create getRecentAppointmentsList in actions and pull data of pending, cancelled and scheduled appts for front end

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,8 +1,13 @@
+import StatCard from '@/components/StatCard'
+import { getRecentAppointmentList } from '@/lib/actions/appointment.actions'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
-const Admin = () => {
+const Admin = async () => {
+  
+  const appointments = await getRecentAppointmentList();
+
   return (
     <div className='mx-auto flex max-w-7xl flex-col space-y-14'>
       <header className='admin-header'>
@@ -17,6 +22,32 @@ const Admin = () => {
         </Link>
         <p className='text-16-semibold'>Admin Dashboard</p>
       </header>
+      <main className='admin-main'>
+        <section className='w-full space-y-4'>
+          <h1 className='header'>Welcome 👋</h1>
+          <p className='text-dark-700'>Manage your appointments here</p>
+        </section>
+        <section className='admin-stat'>
+          <StatCard
+            type="appointments"
+            count={appointments.scheduledCount}
+            label="Scheduled appointments"
+            icon="/assets/icons/appointments.svg"
+          />
+          <StatCard
+            type="pending"
+            count={appointments.pendingCount}
+            label="Pending appointments"
+            icon="/assets/icons/pending.svg"
+          />
+          <StatCard
+            type="cancelled"
+            count={appointments.cancelledCount}
+            label="Cancelled appointments"
+            icon="/assets/icons/cancelled.svg"
+          />
+        </section>
+      </main>
     </div>
   )
 }
