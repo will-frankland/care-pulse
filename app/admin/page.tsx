8ac366f4e5c3e65d36fc2d4a22d9a1d056d6fa8c
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2024-09-26 13:19:10 -0400

    setup header for admin page

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,9 +1,22 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const Admin = () => {
   return (
-    <div>
-      Admin
+    <div className='mx-auto flex max-w-7xl flex-col space-y-14'>
+      <header className='admin-header'>
+        <Link href="/" className='cursor-pointer'>
+        <Image
+          src="/assets/icons/logo-full.svg"
+          alt="logo"
+          height={32}
+          width={162}
+          className='h-8 w-fit'
+        />
+        </Link>
+        <p className='text-16-semibold'>Admin Dashboard</p>
+      </header>
     </div>
   )
 }
